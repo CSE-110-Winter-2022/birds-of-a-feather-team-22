995c2c41ecbda1b5951ecd2e0d5d721ec5f8b2e0
drake <daomar@ucsd.edu>
2022-02-02 03:35:00 -0800

    added spinner and more UI elements

--- app/src/main/java/com/example/birdsofafeather/SetCoursesActivity.java
+++ app/src/main/java/com/example/birdsofafeather/SetCoursesActivity.java
@@ -3,6 +3,8 @@ package com.example.birdsofafeather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SetCoursesActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SetCoursesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_courses);
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner1);
+//create a list of items for the spinner.
+        String[] items = new String[]{"Winter 2018", "Spring 2018", "Fall 2018", "Winter 2019"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
     }
 }

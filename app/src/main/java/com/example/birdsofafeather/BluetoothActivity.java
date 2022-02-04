902a06f315d3d87c2246ad1faa5c7f9830fb2da5
Raul Martinez <raulmar1341@gmail.com>
2022-02-03 20:11:22 -0800

    Added match user layout

--- app/src/main/java/com/example/birdsofafeather/BluetoothActivity.java
+++ app/src/main/java/com/example/birdsofafeather/BluetoothActivity.java
@@ -2,9 +2,15 @@ package com.example.birdsofafeather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class BluetoothActivity extends AppCompatActivity {
@@ -15,15 +21,43 @@ public class BluetoothActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bluetooth);
     }
     public void onClickStart(View view) {
+        ProgressBar progressBar = findViewById(R.id.progressBar4);
+
+        ValueAnimator animator = ValueAnimator.ofInt(0, progressBar.getMax());
+        animator.setDuration(1200);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation){
+                progressBar.setProgress((Integer)animation.getAnimatedValue());
+            }
+        });
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+            }
+        });
+        animator.start();
+
         Button stopButton = findViewById(R.id.stopButton);
         TextView findMatches = findViewById(R.id.textView6);
+        Button startButton = findViewById(R.id.startButton);
 
-        stopButton.setVisibility(View.VISIBLE);
         findMatches.setVisibility(View.VISIBLE);
-        findViewById(R.id.startButton).setVisibility(View.INVISIBLE);
+        stopButton.setVisibility(View.VISIBLE);
+        startButton.setVisibility(View.GONE);
     }
 
     public void onClickStop(View view) {
+        Button stopButton = findViewById(R.id.stopButton);
+        TextView findMatches = findViewById(R.id.textView6);
+        TextView matchesFound = findViewById(R.id.matchesFound);
+        LinearLayout dummy2 = findViewById(R.id.dummy2);
+
+        stopButton.setVisibility(View.GONE);
+        findMatches.setVisibility(View.GONE);
+        matchesFound.setVisibility(View.VISIBLE);
+        dummy2.setVisibility(View.VISIBLE);
 
     }
 }

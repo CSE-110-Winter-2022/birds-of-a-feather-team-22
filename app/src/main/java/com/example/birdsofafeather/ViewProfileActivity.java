98bffcb42852be7d5f4f1dcb31afa046c0db384e
rcjng <robertcjiang@gmail.com>
2022-02-10 16:53:02 -0800

    Updated ViewProfileActivity

--- app/src/main/java/com/example/birdsofafeather/ViewProfileActivity.java
+++ app/src/main/java/com/example/birdsofafeather/ViewProfileActivity.java
@@ -39,36 +39,14 @@ public class ViewProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
 
-        /**comment out for now, until full implementation from HomeScreen*/
-        //Intent intent = getIntent();
-        //int profileId = intent.getIntExtra("profileId", 1);
-
-        int profileId = 3;
-
-        AppDatabase db = AppDatabase.singleton(this);
-        CourseDao courseDao = db.courseDao();
-        ProfileDao profileDao = db.profileDao();
-
-        Profile testProfile = new Profile(3,"Drake", "https://www.google.com");
-        profileDao.insert(testProfile);
+        Intent intent = getIntent();
+        int profileId = intent.getIntExtra("profileId", 1);
 
         //set name
         nameTextView = findViewById(R.id.profile_name_textview);
-        nameTextView.setText(profileDao.getProfile(profileId).getName());
+        nameTextView.setText(databaseTracker.getUserProfile(profileId).getName());
 
-        //List<Course> sharedCourses = databaseTracker.getSharedCourses(profileId);
-        Course testCourse1 = new Course(3,"2016", "Fall","CSE","11");
-        Course testCourse2 = new Course(3,"2017", "Spring","CSE","100");
-        Course testCourse3 = new Course(3,"2017", "Spring","MATH","20");
-        Course testCourse4 = new Course(3,"2018", "Fall","CSE","110");
-        Course testCourse5 = new Course(3,"2018", "Winter","CSE","120");
-        List<Course> sharedCourses = new ArrayList<Course>();
-        
-        sharedCourses.add(testCourse1);
-        sharedCourses.add(testCourse2);
-        sharedCourses.add(testCourse3);
-        sharedCourses.add(testCourse4);
-        sharedCourses.add(testCourse5);
+        List<Course> sharedCourses = databaseTracker.getSharedCourses(profileId);
 
         sharedCoursesRecyclerView = findViewById(R.id.shared_courses_view);
 
@@ -77,30 +55,5 @@ public class ViewProfileActivity extends AppCompatActivity {
 
         viewProfileAdapter = new ViewProfileAdapter(sharedCourses, profileId);
         sharedCoursesRecyclerView.setAdapter(viewProfileAdapter);
-    }
-
-
-    public void onClickStart(View view) {
-
-        Button stopButton = findViewById(R.id.stopButton);
-        TextView findMatches = findViewById(R.id.textView6);
-        Button startButton = findViewById(R.id.startButton);
-
-        findMatches.setVisibility(View.VISIBLE);
-        stopButton.setVisibility(View.VISIBLE);
-        startButton.setVisibility(View.GONE);
-    }
-
-    public void onClickStop(View view) {
-        Button stopButton = findViewById(R.id.stopButton);
-        TextView findMatches = findViewById(R.id.textView6);
-        TextView matchesFound = findViewById(R.id.matchesFound);
-        LinearLayout dummy2 = findViewById(R.id.dummy2);
-
-        stopButton.setVisibility(View.GONE);
-        findMatches.setVisibility(View.GONE);
-        matchesFound.setVisibility(View.VISIBLE);
-        dummy2.setVisibility(View.VISIBLE);
-
     }
 }

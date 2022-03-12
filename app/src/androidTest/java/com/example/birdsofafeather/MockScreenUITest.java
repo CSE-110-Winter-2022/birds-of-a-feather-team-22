cb8dc29081e469f85b7f0d88834ccf8d910c4a72
Raul Martinez <raulmar1341@gmail.com>
2022-03-11 17:40:19 -0800

    Added documentation

--- app/src/androidTest/java/com/example/birdsofafeather/MockScreenUITest.java
+++ app/src/androidTest/java/com/example/birdsofafeather/MockScreenUITest.java
@@ -14,10 +14,13 @@ import androidx.test.filters.LargeTest;
 
 import com.example.birdsofafeather.db.AppDatabase;
 import com.example.birdsofafeather.db.DiscoveredUser;
+import com.example.birdsofafeather.db.Profile;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @LargeTest
 public class MockScreenUITest {
@@ -26,23 +29,54 @@ public class MockScreenUITest {
 
     String EMPTY_STRING = "";
 
-    String UUID_TEST = "a4ca50b6-941b-11ec-b909-0242ac120002,,,,\n";
-    String NAME_TEST = "Bill,,,,\n";
-    String IMAGE_TEST = "something,,,,\n";
-    String COURSE1_TEST = "2021,FA,CSE,110,Large\n";
-    String[] PROFILE_TEST = {UUID_TEST, NAME_TEST, IMAGE_TEST, COURSE1_TEST};
+    String UUID_TEST1 = "a4ca50b6-941b-11ec-b909-0242ac120002,,,,\n";
+    String NAME_TEST1 = "Bill,,,,\n";
+    String IMAGE_TEST1 = "something,,,,\n";
+    String COURSE1_TEST1 = "2021,FA,CSE,110,Large\n";
 
-    public String outputFormattedCSVString() {
+    String UUID_TEST2 = "a4ca50b6-941b-11ec-b909-0242ac120002,,,,\n";
+    String NAME_TEST2 = "Bill,,,,\n";
+    String IMAGE_TEST2 = "something,,,,\n";
+    String COURSE1_TEST2 = "2021,FA,CSE,110,Large\n";
+
+    String UUID_TEST3 = "a4ca50b6-941b-11ec-b909-0242ac120002,,,,\n";
+    String NAME_TEST3 = "Bill,,,,\n";
+    String IMAGE_TEST3 = "something,,,,\n";
+    String COURSE1_TEST3 = "2021,FA,CSE,110,Large\n";
+
+    String[] PROFILE_TEST1 = {UUID_TEST1, NAME_TEST1, IMAGE_TEST1, COURSE1_TEST1};
+    String[] PROFILE_TEST2 = {UUID_TEST2, NAME_TEST2, IMAGE_TEST2, COURSE1_TEST2};
+    String[] PROFILE_TEST3 = {UUID_TEST3, NAME_TEST3, IMAGE_TEST3, COURSE1_TEST3};
+
+    public String outputFormattedCSVString(int num) {
         StringBuilder csvString = new StringBuilder();
-        for (String content : PROFILE_TEST) {
-            csvString.append(content);
-        }
+        switch(num) {
+            case 1:
+                for (String content : PROFILE_TEST1) {
+                    csvString.append(content);
+                }
 
-        return csvString.toString();
+                return csvString.toString();
+            case 2:
+                for (String content : PROFILE_TEST2) {
+                    csvString.append(content);
+                }
+
+                return csvString.toString();
+            case 3:
+                for (String content : PROFILE_TEST3) {
+                    csvString.append(content);
+                }
+
+                return csvString.toString();
+            default:
+                return new StringBuilder().toString();
+        }
     }
 
-    public DiscoveredUser createTestProfile() {
-        return new DiscoveredUser(UUID_TEST, "Something", 1);
+    @Before
+    public void addSelfUser() {
+        db.profileDao().insert(new Profile("1", "Bill", true, false, false, false));
     }
 
     @Test
@@ -59,36 +93,42 @@ public class MockScreenUITest {
     }
 
     @Test
-    public void onClickEnterButtonWithSomeTextTest() {
+    public void enteringOnePersonMockTest() {
         ActivityScenario<MockingActivity> mockScreen = ActivityScenario.launch(MockingActivity.class);
         mockScreen.onActivity(activity -> {
             Button enterButton = activity.findViewById(R.id.mock_enter_button);
             EditText textBox = activity.findViewById(R.id.mocking_input_view);
+            textBox.setText(outputFormattedCSVString(1));
 
-            textBox.setText(outputFormattedCSVString());
-
-            assertEquals(outputFormattedCSVString(), textBox.getText().toString());
+            assertEquals(outputFormattedCSVString(1), textBox.getText().toString());
 
             enterButton.performClick();
 
             assertEquals(EMPTY_STRING, textBox.getText().toString());
-            assertEquals(1, db.discoveredUserDao().count());
         });
     }
 
     @Test
-    public void enterInvalidProfileInfoIntoTextBoxTest() {
+    public void enteringMultipleMockUITest() {
         ActivityScenario<MockingActivity> mockScreen = ActivityScenario.launch(MockingActivity.class);
         mockScreen.onActivity(activity -> {
             Button enterButton = activity.findViewById(R.id.mock_enter_button);
             EditText textBox = activity.findViewById(R.id.mocking_input_view);
 
-            textBox.setText("Something invalid");
-
+            textBox.setText(outputFormattedCSVString(1));
+            assertEquals(outputFormattedCSVString(1), textBox.getText().toString());
             enterButton.performClick();
-
             assertEquals(EMPTY_STRING, textBox.getText().toString());
-            assertEquals(0, db.discoveredUserDao().count());
+
+            textBox.setText(outputFormattedCSVString(2));
+            assertEquals(outputFormattedCSVString(2), textBox.getText().toString());
+            enterButton.performClick();
+            assertEquals(EMPTY_STRING, textBox.getText().toString());
+
+            textBox.setText(outputFormattedCSVString(3));
+            assertEquals(outputFormattedCSVString(3), textBox.getText().toString());
+            enterButton.performClick();
+            assertEquals(EMPTY_STRING, textBox.getText().toString());
         });
     }
 }
